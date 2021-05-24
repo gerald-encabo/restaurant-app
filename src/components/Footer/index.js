6d10e2750900fcaa90aa46e542ff937e619feb12
Gerald Encabo <gerald_encabo@yahoo.com>
2021-05-23 21:12:44 -0400

    initial commit

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,21 +9,21 @@ const Footer = () => {
       <FooterWrap>
         <SocialMedia>
           <SocialMediaWrap>
-            <SocialLogo to='/top'>Ramen &nbsp; <BsFillHouseDoorFill /> &nbsp; House</SocialLogo>
+            <SocialLogo to='/'>Ramen &nbsp; <BsFillHouseDoorFill /> &nbsp; House</SocialLogo>
             <SocialIcons>
-              <SocialIconLink href='/' target='_blank' aria-label='Facebook'>
+              <SocialIconLink href='#' target='_blank' aria-label='Facebook'>
                 <FaFacebook />
               </SocialIconLink>
-              <SocialIconLink href='/' target='_blank' aria-label='Instagram'>
+              <SocialIconLink href='#' target='_blank' aria-label='Instagram'>
                 <FaInstagram />
               </SocialIconLink>
-              <SocialIconLink href='/' target='_blank' aria-label='Youtube'>
+              <SocialIconLink href='#' target='_blank' aria-label='Youtube'>
                 <FaYoutube />
               </SocialIconLink>
-              <SocialIconLink href='/' target='_blank' aria-label='Twitter' rel='noopener noreferrer'>
+              <SocialIconLink href='#' target='_blank' aria-label='Twitter' rel='noopener noreferrer'>
                 <FaTwitter />
               </SocialIconLink>
-              <SocialIconLink href='/' target='_blank' aria-label='Linkedin'>
+              <SocialIconLink href='#' target='_blank' aria-label='Linkedin'>
                 <FaLinkedin />
               </SocialIconLink>
             </SocialIcons>
